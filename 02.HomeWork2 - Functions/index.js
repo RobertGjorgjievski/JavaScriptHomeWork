75ba66cc9f43589af5b954ab08a29d88ee759de9
RobertGjorgjoevski <robertgjorgjievski@outlook.com>
2021-01-15 17:08:07 +0100

    Funtions

---
+++ 02.HomeWork2 - Functions/index.js
@@ -0,0 +1,47 @@
+// HomeWork Part 1
+
+
+function typeOfParameter (x){
+    console.log(typeof x);
+}
+  
+typeOfParameter(5);
+typeOfParameter(null);
+typeOfParameter("Hello");
+typeOfParameter(true);
+typeOfParameter();
+
+
+//HomeWork Part 2
+
+// function calculetDogYears (year){
+//     //debugger;
+//    let dogYear = year * 7;
+
+//     console.log("For" ,year ," humman years,  the dog has " ,dogYear," dogs years.");
+// }
+// function calculetHummanYears (year){
+//     //debugger;
+//     let hummanYears = year / 7;
+//     console.log("For",year ,"dog years, humman have",hummanYears, "years.")
+// }
+// calculetDogYears(3);
+// calculetHummanYears(47);
+
+//HomeWork Part 3
+
+
+//  let moneyInTheBunk = 2000;
+
+//  alert("You have $2000 on your bunk account");
+//  let chas = prompt("How much money do you want?");
+
+//  function moneyLeftInTheBank (){
+//     let moneyleft = moneyInTheBunk - chas;
+//      alert(`You have withdrawn $${chas} and  you have left  $${moneyleft} in your bunk account.`);
+//  }
+
+//  if (moneyInTheBunk >= chas)
+//      moneyLeftInTheBank();
+//   else
+//      alert("Not enough money!!!");
