289873d37f9fd13973515607051f8393e4d1a99a
RobertGjorgjoevski <robertgjorgjievski@outlook.com>
2021-01-15 16:45:48 +0100

    Home Worck ZOdiac

---
+++ 01.HomeWork1/Chinese Zodiac/index.js
@@ -0,0 +1,39 @@
+let year = prompt("Enter your birth year");
+let zodiac = (year - 4) % 12;
+
+if(zodiac == 0 ){
+    alert("You are Rat in Chainese Zodiac");
+} 
+    else if (zodiac == 1 ){
+    alert("You are OX in Chainese Zodiac");
+}
+    else if (zodiac == 2 ){
+    alert("You are Tiger in Chainese Zodiac");
+}
+    else if (zodiac == 3 ){
+    alert("You are Rabbit in Chainese Zodiac");
+}
+    else if (zodiac == 4 ){
+    alert("You are Dragon in Chainese Zodiac");
+}
+    else if (zodiac == 5 ){
+    alert("You are Snake in Chainese Zodiac");
+}
+    else if (zodiac == 6 ){
+    alert("You are Horse in Chainese Zodiac");
+}
+    else if (zodiac == 7 ){
+    alert("You are Goat in Chainese Zodiac");
+}
+    else if (zodiac == 8 ){
+    alert("You are Monkey in Chainese Zodiac");
+}
+    else if (zodiac == 9 ){
+    alert("You are Rooster in Chainese Zodiac");
+}
+    else if (zodiac == 10 ){
+    alert("You are Dog in Chainese Zodiac");
+}
+    else if (zodiac == 11 ){
+    alert("You are Pig in Chainese Zodiac");
+}   
